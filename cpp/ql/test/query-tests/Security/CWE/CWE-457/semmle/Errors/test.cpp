2ee56cd7921116a0d1a61b7b27382a9001d2dd43
Calum Grant <calumgrant@github.com>
2024-09-16 15:21:18 +0100

    C++: Format C++ file

--- cpp/ql/test/query-tests/Security/CWE/CWE-457/semmle/Errors/test.cpp
+++ cpp/ql/test/query-tests/Security/CWE/CWE-457/semmle/Errors/test.cpp
@@ -1,8 +1,7 @@
 // semmle-extractor-options: --expect_errors
 
-int f()
-{
-    int x;
-    initialize(&x); // error expression - initialize() is not defined
-    return x;       // GOOD - assume x is initialized
+int f() {
+  int x;
+  initialize(&x); // error expression - initialize() is not defined
+  return x;       // GOOD - assume x is initialized
 }

5a3ff1eff9cca72e7e71cbf01fc35ceff8005990
Calum Grant <calumgrant@github.com>
2024-09-16 15:10:17 +0100

    C++: Fix FPs in cpp/uninitialized-local

---
+++ cpp/ql/test/query-tests/Security/CWE/CWE-457/semmle/Errors/test.cpp
@@ -0,0 +1,8 @@
+// semmle-extractor-options: --expect_errors
+
+int f()
+{
+    int x;
+    initialize(&x); // error expression - initialize() is not defined
+    return x;       // GOOD - assume x is initialized
+}
